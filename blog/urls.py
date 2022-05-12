a458febe389c3a8586c97cb66c8dd57dd11caf85
itoyuki88 <yuki.kyudai88@gmail.com>
2022-05-12 20:50:30 +0900

    blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.PostListView.as_view(), name='home'),
+    path('home/', views.PostListView.as_view(), name='home'),
 ]
+

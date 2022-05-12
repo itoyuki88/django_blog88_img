ddb666d9b6e56857fae2fb779c8f4a4c201d4b58
itoyuki88 <yuki.kyudai88@gmail.com>
2022-05-12 17:24:44 +0900

    first

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b-^(@c2l4r#l1gqhmnudbnruxeab!#f3zc8zp3e(*_yx$t0_xv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.setting.(locals())
